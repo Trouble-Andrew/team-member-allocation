04836edc03c44296ef53e45f0fa7d19c628419fa
Andrey Terekhov <satyr663@gmail.com>
2022-09-15 21:53:05 +0300

    feat: add react router and add teams page

---
+++ src/Teams.jsx
@@ -0,0 +1,18 @@
+const Teams = ({ selectedTeam, handleTeamSelectionChange }) => {
+  return (
+    <select
+      name="teams"
+      id="teams"
+      className="form-select form-select-lg"
+      value={selectedTeam}
+      onChange={handleTeamSelectionChange}
+    >
+      <option value="TeamA">TeamA</option>
+      <option value="TeamB">TeamB</option>
+      <option value="TeamC">TeamC</option>
+      <option value="TeamD">TeamD</option>
+    </select>
+  );
+};
+
+export default Teams;
